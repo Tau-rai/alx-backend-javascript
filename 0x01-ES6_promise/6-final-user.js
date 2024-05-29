1e4252c252a3722c728d1665e9c4c6b126ea0a8d
Tau <taumasaire@gmail.com>
2024-05-29 19:48:25 +0200

    await async script

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,22 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [];
 
   promises.push(signUpUser(firstName, lastName));
   promises.push(uploadPhoto(fileName));
 
-  try {
-    const results = await Promise.allSettled(promises);
-
-    const profileResults = results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-    return profileResults;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  return Promise.allSettled(promises)
+    .then((results) => {
+      const profileResults = results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
+      return profileResults;
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
 }
