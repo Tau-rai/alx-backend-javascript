c1a341577473f5db6a51fc3b9e69a3fdf62b8d08
Tau <taumasaire@gmail.com>
2024-06-03 20:57:18 +0200

    typed array script

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,9 @@
 export default function getListStudentByLocation(students, city, newGrades) {
-  if (students.filter((student) => student.location === city).length > 0) {
-    return students.map((student) => {
-      const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
-      return {
-        ...student,
-        grade: grade.length ? grade[0].grade : 'N/A',
-      };
-    });
-  }
-  return [];
+  return students.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
+    return {
+      ...student,
+      grade: grade.length ? grade[0].grade : 'N/A',
+    };
+  });
 }
