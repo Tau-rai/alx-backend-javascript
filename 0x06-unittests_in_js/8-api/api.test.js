102cbb8c75b6d596a8da0fba164ee29d994aaece
Tau <taumasaire@gmail.com>
2024-07-26 18:58:02 +0200

    adding basic integration tests modified script

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,24 +1,22 @@
-// basic integration test
+// basic integration tests
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
 
-const request = require('supertest');
-const assert = require('assert');
-const app = require('./api');
+describe('Index Page', () => {
+    const url = 'http://localhost:7865';
 
-describe('GET /', () => {
-    it('checks status code of GET /', (done) => {
-        request(app)
-        .get('/')
-        .expect(200, done);
+    it('should return status 200', (done) => {
+        request(url, (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
     });
 
-    it('checks response message of GET /', (done) => {
-        request(app)
-        .get('/')
-        .end((err, res) => {
-            if (err) return done(err);
-            assert.strictEqual(res.text, 'Welcome to the payment system');
+    it('should return the correct message', (done) => {
+        request(url, (error, response, body) => {
+            expect(body).to.equal('Welcome to the payment system');
             done();
         });
     });
 });
-    
